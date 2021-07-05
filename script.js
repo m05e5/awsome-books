d6bab630af6a5de6b6bcc9b1639bd2d77c08f5f3
Hubert MUGABO <mugberto@gmail.com>
2021-07-05 19:34:56 +0200

    Fix formatting errors

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-if(localStorage.getItem('data') === null){
-    localStorage.setItem('data', '[]');
+if (localStorage.getItem('data') === null) {
+  localStorage.setItem('data', '[]');
 }
 const add_btn = document.getElementById('add');
 // const book = Object.create( {} );
-function Book( id, title, author ) {
+function Book(id, title, author) {
   this.id = id;
   this.title = title;
   this.author = author;
 }
 
 function saveToLocalStorage(bookList) {
-    localStorage.clear();
-    myList = JSON.stringify(bookList);
-    localStorage.setItem('data', myList);
+  localStorage.clear();
+  myList = JSON.stringify(bookList);
+  localStorage.setItem('data', myList);
 }
 
 function getFromLocalStorage() {
-    if(localStorage.getItem('data') !== null){
-        return JSON.parse(localStorage.getItem('data'));
-    }
+  if (localStorage.getItem('data') !== null) {
+    return JSON.parse(localStorage.getItem('data'));
+  }
 }
 add_btn.addEventListener('click', () => {
-    let bookList = getFromLocalStorage();
-    let id = Date.now();
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let book =new Book(id, title, author);
-    bookList.push(book);
-    saveToLocalStorage(bookList);
-    addBooks();
+  let bookList = getFromLocalStorage();
+  let id = Date.now();
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let book = new Book(id, title, author);
+  bookList.push(book);
+  saveToLocalStorage(bookList);
+  addBooks();
 })
+
 function removeBook(id) {
   let bookList = getFromLocalStorage();
   bookList = bookList.filter(book => book.id != id);
@@ -43,9 +44,11 @@ function addBooks() {
   let bookList = getFromLocalStorage();
   const books = document.getElementById('books');
   const li_to_remove = document.querySelectorAll('li')
-  li_to_remove.forEach( (item) => {item.remove()})
+  li_to_remove.forEach((item) => {
+    item.remove()
+  })
 
-  for(let book of bookList ) {
+  for (let book of bookList) {
     const li = document.createElement('li');
     const br = document.createElement('BR');
     const hr = document.createElement('hr');
