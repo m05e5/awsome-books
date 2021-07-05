4b07d90fdf994a1e3c16c998cb86bbb5e29f76ca
Hubert MUGABO <mugberto@gmail.com>
2021-07-05 15:43:42 +0200

    Add a function to remove a book

--- script.js
+++ script.js
@@ -3,17 +3,24 @@ const add_btn = document.getElementById('add');
 const book = Object.create( {} );
 const bookList = [
     {
-        title: 'lorem ipsu',
-        author: 'testeroo testyy'
+      id: 1,
+      title: 'lorem ipsu',
+      author: 'testeroo testyy'
     },
-    {
-        title: 'Second book',
-        author: 'testeroo testyy'
+    { 
+      id: 2,
+      title: 'Second book',
+      author: 'testeroo testyy'
     }
 ];
 
 add_btn.addEventListener('click', () => {
+    book.id = bookList.length + 1;
     book.title = document.getElementById('title').value;
     book.author = document.getElementById('author').value;
     bookList.push(book);
 })
+
+const removeBook = (id) => {
+  bookList = bookList.filter(book => book.id !== id)
+}
